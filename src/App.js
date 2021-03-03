67e2e32b54a0d4882c477bd2efc823b78507e42b
ArizonaPeachTea <zuxingp888@gmail.com>
2021-03-02 23:03:27 -0600

    importing material UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
+import { MenuItem, FormControl, Select } from "@material-ui/core";
 import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      <h1>Let's build a Covid 19 tracker!</h1>
+      <div className="app__header">
+
+      </div>
+      <h1>COVID-19 TRACKER</h1>
+      <FormControl className="app__dropdown">
+        <Select
+          variant="outlined"
+          value="abc"
+        >
+          <MenuItem value="worldwide">Worldwide</MenuItem>
+          <MenuItem value="worldwide">Worldwide</MenuItem>
+          <MenuItem value="worldwide">Worldwide</MenuItem>
+          <MenuItem value="worldwide">Worldwide</MenuItem>
+        </Select>
+      </FormControl>
+
+
     </div>
   );
 }
