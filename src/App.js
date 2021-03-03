d9ad3775a9f13e9f4688f59ea5066436aa47dd99
ArizonaPeachTea <zuxingp888@gmail.com>
2021-03-03 12:46:17 -0600

    getting dropdown menu to work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import './App.css';
 import { MenuItem, FormControl, Select } from "@material-ui/core";
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then(response => response.json())
+        .then(data => {
+          const countries = data.map(country => {
+            return { name: country.country, value: country.countryInfo.iso2 };
+          });
+          setCountries(countries);
+        });
+    };
+    getCountriesData();
+  }, [])
+
   return (
     <div className="App">
       <div className="app__header">
-
+        <h1>COVID-19 TRACKER</h1>
+        <FormControl className="app__dropdown">
+          <Select
+            variant="outlined"
+            value="abc"
+          >
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            {countries.map(country => {
+              return <MenuItem value={country.value}>{country.name}</MenuItem>
+            })}
+          </Select>
+        </FormControl>
       </div>
-      <h1>COVID-19 TRACKER</h1>
-      <FormControl className="app__dropdown">
-        <Select
-          variant="outlined"
-          value="abc"
-        >
-          <MenuItem value="worldwide">Worldwide</MenuItem>
-          <MenuItem value="worldwide">Worldwide</MenuItem>
-          <MenuItem value="worldwide">Worldwide</MenuItem>
-          <MenuItem value="worldwide">Worldwide</MenuItem>
-        </Select>
-      </FormControl>
 
 
     </div>
